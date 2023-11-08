55f2d0275db352ae4797225a1f57110fae574982
victoria-barb8 <150287028+victoria-barb8@users.noreply.github.com>
2023-11-08 17:07:15 -0300

    Create dobro.cpp

---
+++ dobro.cpp
@@ -0,0 +1,20 @@
+// Exercicio 1
+
+#include <iostream>
+
+using namespace std;
+
+int dobro(int N){
+	int d;
+	d = N*2;
+	return d;
+}
+
+int main()
+{
+ int a;
+ cin >> a;
+cout <<dobro(a);
+
+return 0;       
+}
